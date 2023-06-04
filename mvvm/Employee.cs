ead2d85d3d1ea0d29ac6dafb909bf8d28b914e77
Kirill1018 <115730059+Kirill1018@users.noreply.github.com>
2023-06-04 13:26:45 +0300

    Add files via upload

---
+++ mvvm/Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace mvvm
+{
+    enum Payment { salary, hourly_wage, fee }
+    enum Choice { salary, bet }
+    public class Employee : INotifyPropertyChanged
+    {
+        private string name, charge, selection;//имя, тип оплаты и выбор (зарплата или ставка)
+        private int hours;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                On_prop_chang("Name");
+            }
+        }
+        public string Charge
+        {
+            get { return charge; }
+            set
+            {
+                charge = value;
+                On_prop_chang("Charge");
+            }
+        }
+        public string Selection
+        {
+            get { return selection; }
+            set
+            {
+                selection = value;
+                On_prop_chang("Selection");
+            }
+        }
+        public int Hours
+        {
+            get { return hours; }
+            set
+            {
+                hours = value;
+                On_prop_chang("Hours");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void On_prop_chang(string prop_name) { PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop_name)); }
+    }
+}
---
+++ mvvm/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace mvvm
+{
+    public class ViewModel : Employee, IValueConverter
+    {
+        public ViewModel() { }
+        private bool is_shown_obj;
+        public bool Is_shown
+        {
+            get { return is_shown_obj; }
+            set
+            {
+                is_shown_obj = value;
+                On_prop_chang("Is_shown");
+            }
+        }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Payment[] payments = { Payment.fee, Payment.hourly_wage, Payment.salary };
+            Charge = payments[0].ToString();
+            Choice[] choices = { Choice.salary, Choice.bet };
+            Selection = choices[0].ToString();
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) { throw new NotImplementedException(); }
+    }
+}
